7f1759da920c85f0cd9dfafb00bb760b6ab26370
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-09-26 10:49:52 +0530

    Adding Algorithms Questions

--- Leetcode/Algorithms Track/Flood Fill.cpp
+++ Leetcode/Algorithms Track/Flood Fill.cpp
@@ -3,8 +3,7 @@ public:
     bool isValidPos (int x, int y, int X, int Y) {
         return (x >= 0 && y >= 0 && x < X && y < Y);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int SR, int SC, int newColor) {
-        vector <vector <bool>> Visited (image.size(), vector <bool> (image[0].size(), false));
+    void Fill(vector<vector<int>>& image, int SR, int SC, vector <vector <bool>> &Visited) {
         queue <pair <int, int>> Queue;
         Queue.push({SR, SC});
         vector <int> dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
@@ -17,11 +16,22 @@ public:
                     if (image[SR][SC] == image[sr + dx[i]][sc + dy[i]]) {
                         Visited[sr + dx[i]][sc + dy[i]] = true;
                         Queue.push ({sr + dx[i], sc + dy[i]});
-                        image[sr + dx[i]][sc + dy[i]] = newColor;
+                        image[sr + dx[i]][sc + dy[i]] = 0;
                     }
             Queue.pop();
         }
-        image[SR][SC] = newColor;
-        return image;
+        image[SR][SC] = 0;
+        return;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int CountofIslands = 0;
+        vector <vector <bool>> Visited (image.size(), vector <bool> (image[0].size(), false));
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i].size(); j++)
+                if (grid[i][j] == 1) {
+                    Fill (grid, i, j, Visited);
+                    CountofIslands++;
+                }
+        return CountofIslands;
     }
 };
