83be39c13b00708ccb0f6f9cf4df466a2b425041
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-09-26 20:07:19 +0530

    Adding Contest Problems

--- Leetcode/Maximum Difference Between Increasing Elements.java
+++ Leetcode/Maximum Difference Between Increasing Elements.java
@@ -1,10 +1,9 @@
 class Solution {
     public int maximumDifference(int[] nums) {
         int MaxDiff = -1, MinNum = nums[0], i = 1;
-            while (i < nums.length) {
-                if (nums[i] > MinNum)
-                    MaxDiff = Math.max(MaxDiff, nums[i] - MinNum);
-                MinNum = Math.min(MinNum, nums[i++]);
+        while (i < nums.length) {
+            if (nums[i] > MinNum) MaxDiff = Math.max(MaxDiff, nums[i] - MinNum);
+            MinNum = Math.min(MinNum, nums[i++]);
         }
         return MaxDiff;
     }
