0d2721600e970bfa53f0e511e60e0ea8f0d66c57
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-09-26 12:25:53 +0530

    Adding Algorithms Questions

--- Leetcode/Data Structures Track/Maximum Subarray.cpp
+++ Leetcode/Data Structures Track/Maximum Subarray.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        int MaxSum = INT_MIN, i = 0, Sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            Sum += nums[i];
+            MaxSum = max (MaxSum, Sum);
+            Sum = max (Sum, 0);
+        }
+        return MaxSum;
     }
 };
