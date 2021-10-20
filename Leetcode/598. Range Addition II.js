71b02a8a08f42d0bba6db8ed6e73b120203a1978
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-20 08:22:56 +0530

    Adding P598. Range Addition II

---
+++ Leetcode/598. Range Addition II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+ var maxCount = function(m, n, ops) {
+    let X = m, Y = n;
+        for (const op of ops) {
+            X = Math.min(X, op[0]);
+            Y = Math.min(Y, op[1]);
+        }
+    return X * Y;
+};
