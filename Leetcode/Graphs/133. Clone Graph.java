d3b03dc3d3f3cd53d01bf55a6023ed2ed0b7a39e
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-21 12:57:15 +0530

    Adding P133. Clone Graph in Java

---
+++ Leetcode/Graphs/133. Clone Graph.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private HashMap <Node, Node> Map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node != null) {
+            if (Map.containsKey(node)) return Map.get(node);
+            Node temp = new Node(node.val);
+            Map.put(node, temp);
+            for (Node Neighbor : node.neighbors)
+                temp.neighbors.add(cloneGraph(Neighbor));
+            return temp;
+        }
+        return node;
+    }
+}
