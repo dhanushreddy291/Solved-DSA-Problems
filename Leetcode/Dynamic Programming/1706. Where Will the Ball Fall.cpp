ec9400ce7b11192ef46f9d4159fd914d0c30e9ce
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-23 22:18:43 +0530

    Partial Save of P1706. Where Will the Ball Fall

---
+++ Leetcode/Dynamic Programming/1706. Where Will the Ball Fall.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    void Compute(vector<int> &Pos, vector<int> &Answer, bool isFirstRow = false) {
+        if(isFirstRow){
+            int Columns = Pos.size();
+            for (int i = 0; i < Columns; i++) {
+                if (Pos[i] == -1) {
+                    if (i == 0) Answer[i] = -1;
+                    else if (Pos[i] * Pos[i - 1] < 0) Answer[i] = -1;
+                    else Answer[i] = i - 1;
+                } else {
+                    if (i == Columns - 1) Answer[i] = -1;
+                    else if (Pos[i] * Pos[i + 1] < 0) Answer[i] = -1;
+                    else Answer = i + 1;
+                }
+            }
+        } else {
+            int Columns = Pos.size();
+            vector <bool> shouldIVisit(Columns, false);
+            for (int i = 0; i < Columns; i++) {
+                if (Answer[i] != -1) shouldIVisit[Answer[i]] = true;
+            }
+            for (int i = 0; i < Columns; i++) {
+                if (shouldIVisit[i]) {
+                    if (Pos[i] == -1) {
+                        if (i == 0) Answer[i] = -1;
+                        else if (Pos[i] * Pos[i - 1] < 0) Answer[i] = -1;
+                        else Answer[i] = i - 1;
+                    } else {
+                        if (i == Columns - 1) Answer[i] = -1;
+                        else if (Pos[i] * Pos[i + 1] < 0) Answer[i] = -1;
+                        else Answer = i + 1;
+                    }
+                }
+            }
+
+        }
+    }
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int Rows = grid.size(), Columns = grid[0].size();
+        vector <int> Answer(Columns);
+        Compute(grid[0], Answer, true);
+        for (int i = 1; i < Rows; i++) {
+            Compute(grid[i], Answer);
+        }
+        return Answer;
+    }
+};
