eafe09d8847249c35f8f2083457bcbab0ebb905f
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-10-23 22:29:00 +0530

    Adding P1706. Where Will the Ball Fall

--- Leetcode/Dynamic Programming/1706. Where Will the Ball Fall.cpp
+++ Leetcode/Dynamic Programming/1706. Where Will the Ball Fall.cpp
@@ -11,29 +11,32 @@ private:
                 } else {
                     if (i == Columns - 1) Answer[i] = -1;
                     else if (Pos[i] * Pos[i + 1] < 0) Answer[i] = -1;
-                    else Answer = i + 1;
+                    else Answer[i] = i + 1;
                 }
             }
         } else {
             int Columns = Pos.size();
             vector <bool> shouldIVisit(Columns, false);
+            vector <int> Temp(Columns, -1);
             for (int i = 0; i < Columns; i++) {
                 if (Answer[i] != -1) shouldIVisit[Answer[i]] = true;
             }
             for (int i = 0; i < Columns; i++) {
                 if (shouldIVisit[i]) {
                     if (Pos[i] == -1) {
-                        if (i == 0) Answer[i] = -1;
-                        else if (Pos[i] * Pos[i - 1] < 0) Answer[i] = -1;
-                        else Answer[i] = i - 1;
+                        if (i == 0) Temp[i] = -1;
+                        else if (Pos[i] * Pos[i - 1] < 0) Temp[i] = -1;
+                        else Temp[i] = i - 1;
                     } else {
-                        if (i == Columns - 1) Answer[i] = -1;
-                        else if (Pos[i] * Pos[i + 1] < 0) Answer[i] = -1;
-                        else Answer = i + 1;
+                        if (i == Columns - 1) Temp[i] = -1;
+                        else if (Pos[i] * Pos[i + 1] < 0) Temp[i] = -1;
+                        else Temp[i] = i + 1;
                     }
                 }
             }
-
+            for (int i = 0; i < Columns; i++) {
+                if (Answer[i] >= 0) Answer[i] = Temp[Answer[i]];
+            }
         }
     }
 public:
