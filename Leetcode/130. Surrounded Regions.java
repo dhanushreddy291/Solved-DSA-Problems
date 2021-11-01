2758782bab5d5069bdff9c43e97c6073c8c56849
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-11-01 22:40:28 +0530

    Updating P130. Surrounded Regions

--- Leetcode/130. Surrounded Regions.java
+++ Leetcode/130. Surrounded Regions.java
@@ -8,41 +8,26 @@ class Solution {
     private void checkAllTheEdges(char[][] board) {
         int X = board.length, Y = board[0].length;
         for (int i = 0; i < X; i++) {
-            if (board[i][0] == 'O') {
-                board[i][0] = 'S';
-                DFS(board, i, 0, X, Y);
-            }
-            if (board[i][Y - 1] == 'O') {
-                board[i][Y - 1] = 'S';
-                DFS(board, i, Y - 1, X, Y);
-            }
+            if (board[i][0] == 'O') BFS(board, i, 0, X, Y);
+            if (board[i][Y - 1] == 'O') BFS(board, i, Y - 1, X, Y);
         }
         for (int j = 1; j < Y - 1; j++) {
-            if (board[0][j] == 'O') {
-                board[0][j] = 'S';
-                DFS(board, 0, j, X, Y);
-            }
-            if (board[X - 1][j] == 'O') {
-                board[X - 1][j] = 'S';
-                DFS(board, X - 1, j, X, Y);
-            }
+            if (board[0][j] == 'O') BFS(board, 0, j, X, Y);
+            if (board[X - 1][j] == 'O') BFS(board, X - 1, j, X, Y);
         }
     }
-    private void DFS(char[][] board, int x, int y, int X, int Y) {
+    private void BFS(char[][] board, int x, int y, int X, int Y) {
+        board[x][y] = 'S';
         for (int i = 0; i < 4; i++) {
-            if (areValidCoordinates(x + dx[i], y + dy[i], X, Y)) {
-                if (board[x + dx[i]][y + dy[i]] == 'O') {
-                    board[x + dx[i]][y + dy[i]] = 'S';
-                    DFS(board, x + dx[i], y + dy[i], X, Y);
-                }
+            if (areValidCoordinates(x + dx[i], y + dy[i], X, Y) && board[x + dx[i]][y + dy[i]] == 'O') {
+                BFS(board, x + dx[i], y + dy[i], X, Y);
             }
         }
-        return;
     }
     private boolean areValidCoordinates(int x, int y, int X, int Y) {
         return x >= 0 && x < X && y >= 0 && y < Y;
     }
-    private char[][] rewriteTheBoard(char[][] board) {
+    private void rewriteTheBoard(char[][] board) {
         int X = board.length, Y = board[0].length;
         for (int i = 0; i < X; i++) {
             for (int j = 0; j < Y; j++) {
@@ -50,6 +35,5 @@ class Solution {
                 else if (board[i][j] == 'S') board[i][j] = 'O';
             }
         }
-        return board;
     }
 }
