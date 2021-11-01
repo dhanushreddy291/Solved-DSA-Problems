24bf06cbc82dd58da25c8397738805412460d973
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-11-01 12:14:44 +0530

    Adding P897. Increasing Order Search Tree

---
+++ Leetcode/897. Increasing Order Search Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    pair <TreeNode*, TreeNode*> Helper(TreeNode* Root) {
+        if (Root == NULL) return {NULL, NULL};
+        pair <TreeNode*, TreeNode*> L, R;
+        if (Root -> left == NULL) {
+            R = Helper(Root -> right);
+            Root -> right = R.first;
+            return {Root, R.second == NULL ? Root : R.second};
+        }
+        else {
+            L = Helper(Root -> left);
+            L.second -> right = Root;
+            R = Helper(Root -> right);
+            Root -> left = NULL;
+            Root -> right = R.first;
+            return {L.first, R.first == NULL ? Root : R.second};
+        }
+    }
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        if (root == NULL) return NULL;
+        return Helper(root).first;
+    }
+};
