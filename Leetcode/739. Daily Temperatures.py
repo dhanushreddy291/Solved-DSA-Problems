2c7b173692d805d021a4d237dfc6e0d81c1819a2
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-11-13 22:35:16 +0530

    Adding P739. Daily Temperatures

---
+++ Leetcode/739. Daily Temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        Stack = []
+        N = len(temperatures)
+        Stack.append(N - 1)
+        Answer = [0] * N
+        for i in range(N - 2, -1, -1):
+            while len(Stack) > 0 and temperatures[i] >= temperatures[Stack[len(Stack) - 1]]: 
+                Stack.pop()
+            if len(Stack) != 0: 
+                Answer[i] = Stack[len(Stack) - 1] - i
+            Stack.append(i)
+        return Answer
