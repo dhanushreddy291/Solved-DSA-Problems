e47fbbd3fc8d9238230833a8a6b774801d0b7e74
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2021-11-14 22:51:13 +0530

    Updating P638. Shopping Offers

--- Leetcode/638. Shopping Offers.cpp
+++ Leetcode/638. Shopping Offers.cpp
@@ -13,33 +13,6 @@ private:
                 }
             }
             if (!isValid) continue;
-            if (Sum >= special[i][M]) 
-            CorrectOffers.push_back(special[i]);
-        }
-        return;
-    }
-    int FindMinCost(vector<int>& price, vector <vector <int>> &CorrectOffers, vector<int>& needs) {
-        int MinCost = INT_MAX;
-        for (int i = 0; i < CorrectOffers.size(); i++) {
-            bool isValid = true;
-            for (int j = 0; j < needs.size(); j++) {
-                if (needs[j] < CorrectOffers[i][j]) {
-                    isValid = false;
-                    break;class Solution {
-private:
-    void DiscardUnnecessaryOffers(vector<vector<int>>& special, vector<int> &price, vector<vector<int>>& CorrectOffers, vector<int>& needs) {
-        int N = special.size(), M = special[0].size() - 1;
-        for (int i = 0; i < N; i++) {
-            int Sum = 0;
-            bool isValid = true;
-            for (int j = 0; j < M; j++) {
-                Sum += special[i][j] * price[j];
-                if (special[i][j] > needs[j]) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if (!isValid) continue;
             else if (Sum >= special[i][M]) 
                 CorrectOffers.push_back(special[i]);
         }
@@ -78,33 +51,5 @@ public:
         vector <vector <int>> CorrectOffers;
         DiscardUnnecessaryOffers(special, price, CorrectOffers, needs);
         return (CorrectOffers.size() == 0) ? MinCostWhenEmpty(needs, price, needs.size()) : FindMinCost(price, CorrectOffers, needs);
-    }
-};
-                };
-            }
-            if (isValid) {
-                for (int j = 0; j < needs.size(); j++) needs[j] -= CorrectOffers[i][j];
-                MinCost = min(MinCost, CorrectOffers[i].back() + FindMinCost(price, CorrectOffers, needs)); 
-                for (int j = 0; j < needs.size(); j++) needs[j] += CorrectOffers[i][j];
-            } else {
-                int Cost = 0;
-                for (int i = 0; i < needs.size(); i++) 
-                    Cost += needs[i] * price[i]; 
-                MinCost = min(MinCost, Cost);
-            }
-        }
-        return MinCost;
-    }
-public:
-    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-        vector <vector <int>> CorrectOffers;
-        DiscardUnnecessaryOffers(special, price, CorrectOffers, needs);
-        if (CorrectOffers.size() == 0) {
-            int Cost = 0;
-            for (int i = 0; i < needs.size(); i++) 
-                Cost += needs[i] * price[i]; 
-            return Cost;
-        }
-        return FindMinCost(price, CorrectOffers, needs);
     }
 };
