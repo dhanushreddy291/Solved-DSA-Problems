ea4882848b2f42495d1d3b7dea0803ff6672ecb0
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-01-17 12:16:22 +0530

    Adding P2138. Divide a String Into Groups of Size k

--- Leetcode/2138. Divide a String Into Groups of Size k.cpp
+++ Leetcode/2138. Divide a String Into Groups of Size k.cpp
@@ -6,7 +6,7 @@ public:
         for (int i = 0; i < N; i += k) {
             if (i + k - 1 < N) Answer.push_back(s.substr(i, k));
             else {
-                string temp = s.substr(i, k);
+                string temp = s.substr(i);
                 while(temp.size() != k) temp += fill;
                 Answer.push_back(temp);
             }
