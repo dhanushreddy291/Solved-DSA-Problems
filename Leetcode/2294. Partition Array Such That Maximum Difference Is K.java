d88a87491437b10880ef373f1b6f7e863dfc06ec
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-06 14:53:58 +0530

    Adding P2294. Partition Array Such That Maximum Difference Is K

--- Leetcode/2294. Partition Array Such That Maximum Difference Is K.java
+++ Leetcode/2294. Partition Array Such That Maximum Difference Is K.java
@@ -1,11 +1,10 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        if (k == 0) return new HashSet<>(Arrays.asList(nums)).size();
         int partitions = 0, Min = nums[0], Max = nums[0];
-        for (int &num : nums) {
-            Min = min(Min, num);
-            Max = max(Max, num);
+        for (int num : nums) {
+            Min = Math.min(Min, num);
+            Max = Math.max(Max, num);
             if (Max - Min > k) {
                 partitions++;
                 Min = Max = num;
