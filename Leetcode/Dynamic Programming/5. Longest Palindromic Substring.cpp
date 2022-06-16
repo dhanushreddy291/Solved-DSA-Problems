e4414f503b1f235f303b867140088b4fe6157b9a
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-16 12:18:53 +0530

    Adding DP Approach

--- Leetcode/Dynamic Programming/5. Longest Palindromic Substring.cpp
+++ Leetcode/Dynamic Programming/5. Longest Palindromic Substring.cpp
@@ -19,3 +19,26 @@ public:
         return maxLength & 1 ? s.substr(index - maxLength/2, maxLength) : s.substr(index - maxLength/2 + 1, maxLength);
     }
 };
+
+
+// DP Solution
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int N = s.size(), start = 0, maxLength = 0;
+        vector <vector <bool>> DP(N, vector <bool> (N));
+        for (int length = 0; length < N; length++) {
+            for (int i = 0, j = length; j < N; i++, j++) {
+                if (i == j) DP[i][j] = true;
+                else if (length == 1) DP[i][j] = (s[i] == s[j]);
+                else DP[i][j] = (s[i] == s[j] && DP[i + 1][j - 1]);
+                if (DP[i][j] and j - i + 1 > maxLength) {
+                    maxLength = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substr(start, maxLength);
+    }
+};
